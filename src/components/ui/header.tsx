ba5bb7b2c0071b9bbcac00c120cc49c74915a8b0
MarlonChi <marlonchiodelli@hotmail.com>
2023-10-17 14:14:16 -0300

    feat: header component

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { Card } from './card'
+import { Button } from './button'
+import { MenuIcon, ShoppingCartIcon } from 'lucide-react'
+
+const Header = () => {
+  return (
+    <Card className='flex justify-between items-center p-[1.875rem]'>
+      <Button size="icon" variant='outline'>
+        <MenuIcon />
+      </Button>
+
+      <h1 className='font-semibold text-lg'><span className='text-primary'>FSW</span> Store</h1>
+
+      <Button size="icon" variant='outline'>
+        <ShoppingCartIcon />
+      </Button>
+    </Card>
+  )
+}
+
+export default Header
